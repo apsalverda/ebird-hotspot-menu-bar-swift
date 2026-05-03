873642ae74ccd572ddfa70ff14fbbb10e87ffd51
Anne Pier Salverda <salverda@yahoo.com>
2026-05-03 09:09:22 -0400

    Initial Commit

---
+++ ebird-hotspot-menubar/ebird_hotspot_menubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebird_hotspot_menubarApp.swift
+//  ebird-hotspot-menubar
+//
+//  Created by Anne Salverda on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ebird_hotspot_menubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
